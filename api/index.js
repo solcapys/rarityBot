9cef4b1b60b56fbad71dd56cda8238b40907fb05
mata <mata.perez.luis@gmail.com>
2021-10-13 11:08:01 -0300

    cambios

--- api/index.js
+++ api/index.js
@@ -2,13 +2,8 @@ const {
   InteractionResponseType,
   InteractionType,
   verifyKey,
-} = require('discord-interactions');
-const getRawBody = require('raw-body');
-var fs = require('fs');
-
-const path = require("path");
-
-var obj = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../csvjson.json'), 'utf8'));
+} = require("discord-interactions");
+const getRawBody = require("raw-body");
 
 const RARITYCAPY_COMMAND = {
   name: "rarity",
@@ -23,17 +18,19 @@ const RARITYCAPY_COMMAND = {
   ],
 };
 
-var found = 0; 
 const INVITE_URL = `https://discord.com/oauth2/authorize?client_id=${process.env.APPLICATION_ID}&scope=applications.commands`;
+
 /**
  * Gotta see someone 'bout a trout
  * @param {VercelRequest} request
  * @param {VercelResponse} response
  */
 module.exports = async (request, response) => {
-  if (request.method === 'POST') {
-    const signature = request.headers['x-signature-ed25519'];
-    const timestamp = request.headers['x-signature-timestamp'];
+  // Only respond to POST requests
+  if (request.method === "POST") {
+    // Verify the request
+    const signature = request.headers["x-signature-ed25519"];
+    const timestamp = request.headers["x-signature-timestamp"];
     const rawBody = await getRawBody(request);
 
     const isValidRequest = verifyKey(
@@ -44,49 +41,39 @@ module.exports = async (request, response) => {
     );
 
     if (!isValidRequest) {
-      console.error('Invalid Request');
-      return response.status(401).send({ error: 'Bad request signature ' });
+      console.error("Invalid Request");
+      return response.status(401).send({ error: "Bad request signature " });
     }
 
+    // Handle the request
     const message = request.body;
 
+    // Handle PINGs from Discord
     if (message.type === InteractionType.PING) {
-      console.log('Handling Ping request');
+      console.log("Handling Ping request");
       response.send({
         type: InteractionResponseType.PONG,
       });
     } else if (message.type === InteractionType.APPLICATION_COMMAND) {
+      // Handle our Slash Commands
       switch (message.data.name.toLowerCase()) {
         case RARITYCAPY_COMMAND.name.toLowerCase():
-
-        found = obj.findIndex(element => element.capy == `capys ${message.data.options[0].value}`);
-
-        var salida='';
-        if(found<0)
-        {
-            salida = "not found";
-        }
-        else{
-            salida = `capy rank ${(found+1)}`;
-        }
-
-        response.status(200).send({
-          type: 4,
-          data: {
-            content: salida,
-          },
-        });
-        
-        server.log.info('rarity Request');
-        break;
+          response.status(200).send({
+            type: 4,
+            data: {
+              content: "Hello!",
+            },
+          });
+          console.log("Slap Request");
+          break;
         default:
-          console.error('Unknown Command');
-          response.status(400).send({ error: 'Unknown Type' });
+          console.error("Unknown Command");
+          response.status(400).send({ error: "Unknown Type" });
           break;
       }
     } else {
-      console.error('Unknown Type');
-      response.status(400).send({ error: 'Unknown Type' });
+      console.error("Unknown Type");
+      response.status(400).send({ error: "Unknown Type" });
     }
   }
 };
