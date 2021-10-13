770bdebae579f62f27f7336aa5ddbdf045a57bd0
mata <mata.perez.luis@gmail.com>
2021-10-13 04:39:43 -0300

    cambios

--- api/index.js
+++ api/index.js
@@ -31,38 +31,34 @@ const INVITE_URL = `https://discord.com/oauth2/authorize?client_id=${process.env
  * @param {VercelRequest} request
  * @param {VercelResponse} response
  */
-
-
 module.exports = async (request, response) => {
-  // We don't want to check GET requests to our root url
   if (request.method === 'POST') {
     const signature = request.headers['x-signature-ed25519'];
     const timestamp = request.headers['x-signature-timestamp'];
+    const rawBody = await getRawBody(request);
+
     const isValidRequest = verifyKey(
-      request.rawBody,
+      rawBody,
       signature,
       timestamp,
       process.env.PUBLIC_KEY
     );
+
     if (!isValidRequest) {
-      server.log.info('Invalid Request');
+      console.error('Invalid Request');
       return response.status(401).send({ error: 'Bad request signature ' });
     }
-  }
-});
 
-  const message = request.body;
+    const message = request.body;
 
-  if (message.type === InteractionType.PING) {
-    server.log.info("Handling Ping request");
-    response.send({
-      type: InteractionResponseType.PONG,
-    });
-
+    if (message.type === InteractionType.PING) {
+      console.log('Handling Ping request');
+      response.send({
+        type: InteractionResponseType.PONG,
+      });
     } else if (message.type === InteractionType.APPLICATION_COMMAND) {
-    console.log(message.data.name.toLowerCase());
-    switch (message.data.name.toLowerCase()) {
-      case RARITYCAPY_COMMAND.name.toLowerCase():
+      switch (message.data.name.toLowerCase()) {
+        case RARITYCAPY_COMMAND.name.toLowerCase():
 
         found = obj.findIndex(element => element.capy == `capys ${message.data.options[0].value}`);
 
@@ -84,15 +80,16 @@ module.exports = async (request, response) => {
         
         server.log.info('rarity Request');
         break;
-      default:
-        server.log.error('Unknown Command');
-        response.status(400).send({ error: 'Unknown Type' });
-        break;
+        default:
+          console.error('Unknown Command');
+          response.status(400).send({ error: 'Unknown Type' });
+          break;
+      }
+    } else {
+      console.error('Unknown Type');
+      response.status(400).send({ error: 'Unknown Type' });
     }
-  } else {
-    server.log.error("Unknown Type");
-    response.status(400).send({ error: "Unknown Type" });
   }
-);
+};
 
 
